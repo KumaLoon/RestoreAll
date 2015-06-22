7d9a4f1e59e793f99e12bc61d380254eea2a1d56
LooneySJ <win7seon@hotmail.com>
2015-06-21 20:08:27 -0400

    Optimized the processes
    Changed .zip-> .tar.gz.
    Changed some codes for the restore feature.

--- Testing scripts/RestoreAll SMS.sh
+++ Testing scripts/RestoreAll SMS.sh
@@ -3,14 +3,11 @@
 #  RestoreAll SMS.sh
 #
 #  Created by LooneySJ
-#  Only Backup is available right now. Restore will be added later.
 
-cd ~/Documents/RestoreAll
+cd ~/Documents
 echo "Exporting Messages"
-mkdir SMS
-echo "You should see a folder named SMS"
 expect -c"
-spawn scp -P 22 -oStrictHostKeyChecking=no -oConnectTimeout=10 -r root@$SSHIP:/var/mobile/Library/SMS SMS
+spawn scp -P 22 -oStrictHostKeyChecking=no -oConnectTimeout=10 -r root@$SSHIP:/var/mobile/Library/SMS RestoreAll
 expect \"root@$SSHIP's password:\"
 stty -echo
 send \"$SSHROOT\r\"
@@ -22,6 +19,7 @@ exit
 "
 clear
 
+cd ~/Documents/RestoreAll
 # STILL TESTING, PLANING TO PUT A STOREAGE WARNING HERE.
 read -p "SMS can be encrypted, do you want to encrypt your messages? (Y/N): " ECSMS
 if [ "$ECSMS" == "Y" ]
@@ -29,13 +27,16 @@ then
 # I'M SERIOUS ABOUT THIS, REMEMBER YOUR PASSWORD!!!
 echo "NOTE: This program will NOT SAVE your encryption password, know your password, or you will have to Brute-force to unzip the messages."
 sleep 4s
-echo "Zipping messages"
-zip -r SMS-E SMS
+echo "Compressing messages (F is tar.gz)"
+tar -zcvf SMS-E.tar.gz SMS
+clear
 echo "Encrypting messages. Please type your password for zip."
-zip -er SMS-E.zip ~/Documents/RestoreAll
+zip -er SMS-E.tar.gz.zip SMS-E.tar.gz
 echo "Done"
 exit
 else
+echo "Compressing messages (F is tar.gz)"
+tar -zcvf SMS.tar.gz SMS
 echo "Done"
 exit
 fi
