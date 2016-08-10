82c52786ffb9499ccdec96d7e14188edaa6f8032
LooneySJ <LSJ@Seons-MacBook-Pro.local>
2016-08-10 03:41:10 -0400

    Fixed and added some codes to work
    - Sleep 10s -> 3s
    - Checked value has been added
    - More Organized

--- fixitplz.sh
+++ fixitplz.sh
@@ -1,41 +1,49 @@
 #!/bin/bash
 
-# Not even beta, not even tested yet
-
 if [ -d /var/mobile/Assests ]; then
-echo "Fixing Library"
-rm -rf Library/Assets
-mv Assets /var/mobile/Library
-clear
+    echo "Fixing Library"
+    rm -rf /var/mobile/Library/Assets
+    mv Assets /var/mobile/Library
+    clear
+    CHECKED="1"
 else
-clear
+    :
 fi
 
 if [ -d /var/mobile/Caches ]; then
-echo "Fixing Library"
-rm -rf Library/Caches
-mv Caches /var/mobile/Library
-clear
+    echo "Fixing Library"
+    rm -rf /var/mobile/Library/Caches
+    mv Caches /var/mobile/Library
+    clear
+    CHECKED="2"
 else
-clear
+    :
 fi
 
 if [ -d /var/mobile/DCIM ]; then
+    :
 else
-clear
-echo "You have to re-run 'Restore Photos'"
-sleep 10s
-clear
+    clear
+    echo "You have to re-run 'Restore Photos'"
+    sleep 3s
+    clear
+    CHECKED="3"
 fi
 
 if [ -d /var/mobile/PhotoData ]; then
+    :
 else
-clear
-echo "You have to re-run 'Restore Photos'. If you are running iOS 9, just ignore this."
-sleep 10s
-clear
+    clear
+    echo "You have to re-run 'Restore Photos'. If you are running iOS 9, just ignore this."
+    sleep 3s
+    clear
+    CHECKED="4"
 fi
 
-if
-
-exit 0
+if [[ "$CHECKED" == "1" && "$CHECKED" == "2" && "$CHECKED" == "3" && "$CHECKED" == "4" ]]; then
+    echo "You should be good to go."
+    exit 0
+else
+    echo "Nothing is wrong here, why are you here?"
+    exit 0
+fi
